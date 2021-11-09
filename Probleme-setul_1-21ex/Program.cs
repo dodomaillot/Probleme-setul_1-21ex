12d64299a183255c2dacfe311fe6c266e230e0d2
dodomaillot <dorianmaillot@gmail.com>
2021-11-09 18:29:53 +0200

    Exercice 1 done

--- Probleme-setul_1-21ex/Program.cs
+++ Probleme-setul_1-21ex/Program.cs
@@ -6,7 +6,25 @@ namespace Probleme_setul_1_21ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Wich exercice do you want to do ? Exercice: ");
+            int exercice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (exercice)
+            {
+                case 1:
+                    Exercice_1();
+                    break;
+            }
         }
+
+        private static void Exercice_1()
+        {
+            int a, b;
+            Console.WriteLine("ax + b = 0");
+            Console.Write("a = "); a = int.Parse(Console.ReadLine());
+            Console.Write("b = "); b = int.Parse(Console.ReadLine());
+            Console.Write("=> x = " + -b / a);
+        }
+
     }
 }
