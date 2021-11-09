7ffb9be3be3c08567f7522a04c844b85d60a1303
dodomaillot <dorianmaillot@gmail.com>
2021-11-09 17:58:57 +0200

    Ajoutez des fichiers projet.

---
+++ Probleme-setul_1-21ex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Probleme_setul_1_21ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
